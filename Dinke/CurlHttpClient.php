792eba3dd30ad61d2ba1ff1f04689c1f8bc84689
Ivan Dilber <ivan@dilber.info>
2014-12-28 12:51:13 +0100

    Force cURL to use TLSv1 for secure connections
    On some distros cURL will use SSLv3 by default (e.g. on Debian7 with
    the latest updates currently available). Many servers are blocking
    secure connections that use SSLv3 to prevent heartbleed attack, and
    you will get curl error #35. To prevent it, we have to force cURL to
    use TLSv1, which it should use anyway. This is the newest version that
    can safely be enforced, as TLSv1_0 and higher require curl 7.34+.

--- Dinke/CurlHttpClient.php
+++ Dinke/CurlHttpClient.php
@@ -48,7 +48,8 @@ class CurlHttpClient
 							CURLOPT_ENCODING		=> 'gzip, deflate',     //The contents of the Accept-Encoding header in curl request
 							CURLOPT_SSL_VERIFYPEER	=> false, 			    //whether to verify ssl peer's  certificate
 							CURLOPT_HEADER			=> false,			    //whether to add response headers to the output
-							CURLOPT_USERAGENT		=> 'CurlHttpClient/v2.0'//default user agent if none is set
+							CURLOPT_USERAGENT		=> 'CurlHttpClient/v2.0',//default user agent if none is set
+							CURLOPT_SSLVERSION		=> 1,				     //force cURL to use TLSv1 (stop it from using SSL)
 			);
 
 	/**
